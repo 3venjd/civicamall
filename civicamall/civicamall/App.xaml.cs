cc6f9f5cc2c8ba2f24c696b4aedb2ab3449ef6f3
3venjd <lui.eve.jim.dur@gmail.com>
2020-06-12 17:20:07 -0500

    Master Detail Added

---
+++ civicamall/civicamall/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using civicamall.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace civicamall
+{
+    public partial class App : Application
+    {
+        public static object Navigator { get; internal set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MasterPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
