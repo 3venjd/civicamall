7af45b43f2d7cee1cd5cbc355c8a4443561a8534
3venjd <lui.eve.jim.dur@gmail.com>
2020-06-23 23:06:30 -0500

    Custom control with binding functionaly

--- civicamall/civicamall/Controls/HeaderButtons.xaml.cs
+++ civicamall/civicamall/Controls/HeaderButtons.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using civicamall.Views;
 using GalaSoft.MvvmLight.Command;
+using System;
 using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -9,25 +10,9 @@ namespace civicamall.Controls
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HeaderButtons : ContentView
     {
-        public ICommand GoBackCommand { get { return new RelayCommand(GoBack); } }
-
-        public ICommand GoToCarCommand { get { return new RelayCommand(GoCar); } }
-
         public HeaderButtons()
         {
             InitializeComponent();
-        }
-
-        private async void GoCar()
-        {
-            var mdp = Application.Current.MainPage as MasterDetailPage;
-            await mdp.Detail.Navigation.PushAsync(new ShoppingCarPage());
-        }
-
-        private async void GoBack()
-        {
-            var mdp = Application.Current.MainPage as MasterDetailPage;
-            await mdp.Detail.Navigation.PopAsync();
         }
     }
 }
---
+++ civicamall/civicamall/Controls/HeaderControlViewModel.cs
@@ -0,0 +1,31 @@
+﻿using civicamall.Views;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace civicamall.Controls
+{
+    public class HeaderControlViewModel
+    {
+        public ICommand GoBackCommand { get { return new RelayCommand(GoBack); } }
+
+        public ICommand GoToCarCommand { get { return new RelayCommand(GoCar); } }
+
+        public HeaderControlViewModel()
+        {
+
+        }
+
+        private async void GoCar()
+        {
+            var mdp = Application.Current.MainPage as MasterDetailPage;
+            await mdp.Detail.Navigation.PushAsync(new ShoppingCarPage());
+        }
+
+        private async void GoBack()
+        {
+            var mdp = Application.Current.MainPage as MasterDetailPage;
+            await mdp.Detail.Navigation.PopAsync();
+        }
+    }
+}
