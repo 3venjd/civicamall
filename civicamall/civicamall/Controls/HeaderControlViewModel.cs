d4fb17a4dc6970e90db2feef8aff585e2f8f6dcc
luis jimenez <eve_ghetto@hotmail.com>
2020-08-10 11:31:33 -0500

    Index custom controll done

--- civicamall/civicamall/Controls/HeaderControlViewModel.cs
+++ civicamall/civicamall/Controls/HeaderControlViewModel.cs
@@ -31,9 +31,6 @@ namespace civicamall.Controls
 
         public ICommand GoNotFoundCommand { get { return new RelayCommand(GoNotFound); } }
 
-
-
-
         private int _position;
 
         private double _opacity;
@@ -94,79 +91,7 @@ namespace civicamall.Controls
                     Image = "promocion_5.jpg"
                 },
             };
-            /*Products = new ObservableCollection<Product>
-            {
-                new Product
-                {
-                     Name = "Zapatillas",
-                     Image = "zapatillas_1",
-                     Brand = "Nike",
-                     Icon_brand = "ic_nike.jpg",
-                     Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
-                                   "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s," +
-                                   " when an unknown printer took a galley of type and scrambled it to make a type " +
-                                   "specimen book. It has survived not only five centuries, but also the leap into " +
-                                   "electronic typesetting, remaining essentially unchanged. It was popularised in " +
-                                   "the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, " +
-                                   "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                     Base_cost = 50000,
-                     Discount = 0,
-                     IsFavorite = true,
-                     IsDiscount = false
-
-                },
-                new Product
-                {
-                     Name = "Zapatillas",
-                     Image = "zapatillas_2",
-                     Base_cost = 90000,
-                     Discount = 0,
-                     IsFavorite = false,
-                     IsDiscount = false
-
-                },
-                new Product
-                {
-                     Name = "Zapatillas",
-                     Image = "zapatillas_3",
-                     Base_cost = 40000,
-                     Discount = 30,
-                     IsFavorite = false,
-                     IsDiscount = true
-
-                },
-                new Product
-                {
-                     Name = "Zapatillas",
-                     Image = "zapatillas_4",
-                     Base_cost = 90000,
-                     Discount = 40,
-                     IsFavorite = false,
-                     IsDiscount = true
-
-                },
-                new Product
-                {
-                     Name = "Zapatillas",
-                     Image = "zapatillas_5",
-                     Base_cost = 110000,
-                     Discount = 20,
-                     IsFavorite = true,
-                     IsDiscount = true
-
-                },
-                new Product
-                {
-                     Name = "Zapatillas",
-                     Image = "zapatillas_6",
-                     Base_cost = 150000,
-                     Discount = 0,
-                     IsFavorite = true,
-                     IsDiscount = false
-
-                },
-
-            }; */
+            
             Malls = new ObservableCollection<Mall>
             {
                 new Mall
--- civicamall/civicamall/Controls/ProductCustomControl.xaml.cs
+++ civicamall/civicamall/Controls/ProductCustomControl.xaml.cs
@@ -1,10 +1,4 @@
-﻿using civicamall.Models;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -14,34 +8,13 @@ namespace civicamall.Controls
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProductCustomControl : ContentView
     {
-
-        public string ProductImage
-        {
-            get { return base.GetValue(ProductImageProperty).ToString(); }
-            set { base.SetValue(ProductImageProperty, value); }
-        }
-
-        public string ProductName
-        {
-            get { return base.GetValue(ProductNameProperty).ToString(); }
-            set { base.SetValue(ProductNameProperty, value); } 
-        }
-
-        public string ProductFinalCost 
-        { 
-            get { return base.GetValue(ProductFinalCostProperty).ToString(); }
-            set { base.SetValue(ProductFinalCostProperty, value); } 
-        }
-
-
-
         public static readonly BindableProperty ProductImageProperty =
             BindableProperty.Create
             (
                 propertyName: "ProductUrlImage",
                 defaultValue: String.Empty,
                 returnType: typeof(string),
-                declaringType:typeof(ProductCustomControl),
+                declaringType: typeof(ProductCustomControl),
                 defaultBindingMode: BindingMode.TwoWay,
                 propertyChanged: PhotoUrlPropertyChanged
             );
@@ -59,14 +32,50 @@ namespace civicamall.Controls
         public static readonly BindableProperty ProductFinalCostProperty =
             BindableProperty.Create
             (
-                propertyName : "FinalCost",
+                propertyName: "FinalCost",
                 defaultValue: string.Empty,
                 returnType: typeof(string),
                 declaringType: typeof(ProductCustomControl),
                 defaultBindingMode: BindingMode.TwoWay,
                 propertyChanged: ProductFinalCostChanged
             );
-        private static void PhotoUrlPropertyChanged(BindableObject bindable, object oldValue,object newValue)
+
+        public static readonly BindableProperty IsFavoriteProperty =
+            BindableProperty.Create
+            (
+                propertyName: "Favorite",
+                defaultValue: false,
+                returnType: typeof(bool),
+                declaringType: typeof(ProductCustomControl),
+                defaultBindingMode: BindingMode.TwoWay,
+                propertyChanged: ProducIsFavoritePropertyChanged
+            );
+
+
+        public string ProductImage
+        {
+            get { return base.GetValue(ProductImageProperty).ToString(); }
+            set { base.SetValue(ProductImageProperty, value); }
+        }
+
+        public string ProductName
+        {
+            get { return base.GetValue(ProductNameProperty).ToString(); }
+            set { base.SetValue(ProductNameProperty, value); }
+        }
+
+        public string ProductFinalCost
+        {
+            get { return base.GetValue(ProductFinalCostProperty).ToString(); }
+            set { base.SetValue(ProductFinalCostProperty, value); }
+        }
+
+        public bool IsFavorite
+        {
+            get { return (bool)base.GetValue(IsFavoriteProperty); }
+            set { base.SetValue(IsFavoriteProperty, value); }
+        }
+        private static void PhotoUrlPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
             var ProductControl = (ProductCustomControl)bindable;
 
@@ -87,13 +96,18 @@ namespace civicamall.Controls
             ProductControl.ProductNameLabel.Text = (string)newValue;
         }
 
-        private static void ProductFinalCostChanged(BindableObject bindable,object oldValue, object newValue)
+        private static void ProductFinalCostChanged(BindableObject bindable, object oldValue, object newValue)
         {
             var ProductControl = (ProductCustomControl)bindable;
 
             ProductControl.ProductFinalCostLabel.Text = (string)newValue;
         }
 
+        private static void ProducIsFavoritePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var ProductControl = (ProductCustomControl)bindable;
+            ProductControl.PancakeViewFavorite.IsVisible = (bool)newValue;
+        }
         public ProductCustomControl()
         {
             InitializeComponent();
