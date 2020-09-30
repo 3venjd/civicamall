547f14c416589433c8bcfd5d53005ab142bd9fd2
luis jimenez <eve_ghetto@hotmail.com>
2020-09-29 22:35:45 -0500

    On Boarding Page Added

---
+++ civicamall/civicamall/ViewModels/OnBoardingViewModel.cs
@@ -0,0 +1,26 @@
+﻿using civicamall.Views;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace civicamall.ViewModels
+{
+    public class OnBoardingViewModel : BaseViewModel
+    {
+        public ICommand GoToLoginCommand { get { return new RelayCommand(GoToLogin); } }
+
+        public OnBoardingViewModel()
+        {
+
+        }
+
+        private async void GoToLogin()
+        {
+            Application.Current.MainPage = new NavigationPage(new LoginPage());
+            await Application.Current.MainPage.Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
