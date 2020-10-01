71233ac0a5aab7c03cff428d32ce05e8e08473dd
luis jimenez <eve_ghetto@hotmail.com>
2020-10-01 16:30:09 -0500

    banners update

---
+++ civicamall.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace civicamall.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ civicamall/civicamall/Renders/CustomEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace civicamall.Renders
+{
+    public class CustomEntry : Entry
+    {
+        public CustomEntry()
+        {
+            BackgroundColor = Color.Red;
+        }
+    }
+}
