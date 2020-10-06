a20114d485205cb7b68ed694fc5995c2af097c1b
luis jimenez <eve_ghetto@hotmail.com>
2020-10-06 13:35:13 -0500

    Text size augmentation

--- civicamall/civicamall.Android/Resources/Resource.designer.cs
+++ civicamall/civicamall.Android/Resources/Resource.designer.cs
@@ -8385,250 +8385,256 @@ namespace civicamall.Droid
 			public const int facebook_logo = 2131165317;
 			
 			// aapt resource value: 0x7F070086
-			public const int facebook_purple_logo_button = 2131165318;
+			public const int facebook_purple_logo = 2131165318;
 			
 			// aapt resource value: 0x7F070087
-			public const int favorite_24px = 2131165319;
+			public const int facebook_purple_logo_button = 2131165319;
 			
 			// aapt resource value: 0x7F070088
-			public const int favorite_purple_24px = 2131165320;
+			public const int favorite_24px = 2131165320;
 			
 			// aapt resource value: 0x7F070089
-			public const int favorite_white_24px = 2131165321;
+			public const int favorite_purple_24px = 2131165321;
 			
 			// aapt resource value: 0x7F07008A
-			public const int filter_list_24px = 2131165322;
-			
-			// aapt resource value: 0x7F07008C
-			public const int Gorras = 2131165324;
+			public const int favorite_white_24px = 2131165322;
 			
 			// aapt resource value: 0x7F07008B
-			public const int gorra_1 = 2131165323;
+			public const int filter_list_24px = 2131165323;
 			
 			// aapt resource value: 0x7F07008D
-			public const int hadware_store = 2131165325;
+			public const int Gorras = 2131165325;
+			
+			// aapt resource value: 0x7F07008C
+			public const int gorra_1 = 2131165324;
+			
+			// aapt resource value: 0x7F07008E
+			public const int hadware_store = 2131165326;
 			
 			// aapt resource value: 0x7F070008
 			public const int Historial = 2131165192;
 			
-			// aapt resource value: 0x7F070091
-			public const int ice_cream = 2131165329;
-			
-			// aapt resource value: 0x7F07008E
-			public const int ic_mtrl_chip_checked_black = 2131165326;
+			// aapt resource value: 0x7F070092
+			public const int ice_cream = 2131165330;
 			
 			// aapt resource value: 0x7F07008F
-			public const int ic_mtrl_chip_checked_circle = 2131165327;
+			public const int ic_mtrl_chip_checked_black = 2131165327;
 			
 			// aapt resource value: 0x7F070090
-			public const int ic_mtrl_chip_close_circle = 2131165328;
+			public const int ic_mtrl_chip_checked_circle = 2131165328;
 			
-			// aapt resource value: 0x7F070092
-			public const int instagram_logo = 2131165330;
+			// aapt resource value: 0x7F070091
+			public const int ic_mtrl_chip_close_circle = 2131165329;
+			
+			// aapt resource value: 0x7F070093
+			public const int instagram_logo = 2131165331;
+			
+			// aapt resource value: 0x7F070094
+			public const int instagram_purple_logo = 2131165332;
 			
 			// aapt resource value: 0x7F070009
 			public const int Lista_de_deseados = 2131165193;
 			
-			// aapt resource value: 0x7F070093
-			public const int local_activity_24px = 2131165331;
-			
-			// aapt resource value: 0x7F070094
-			public const int local_mall_24px = 2131165332;
-			
 			// aapt resource value: 0x7F070095
-			public const int Mayorca_logo = 2131165333;
+			public const int local_activity_24px = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int menu_24px = 2131165334;
+			public const int local_mall_24px = 2131165334;
+			
+			// aapt resource value: 0x7F070097
+			public const int Mayorca_logo = 2131165335;
+			
+			// aapt resource value: 0x7F070098
+			public const int menu_24px = 2131165336;
 			
 			// aapt resource value: 0x7F07000A
 			public const int Mesa_de_trabajo = 2131165194;
 			
-			// aapt resource value: 0x7F070097
-			public const int Mic_white = 2131165335;
-			
-			// aapt resource value: 0x7F070098
-			public const int Moda = 2131165336;
-			
 			// aapt resource value: 0x7F070099
-			public const int Motos = 2131165337;
+			public const int Mic_white = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int mtrl_snackbar_background = 2131165338;
+			public const int Moda = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int mtrl_tabs_default_indicator = 2131165339;
+			public const int Motos = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int navigate_next_24px = 2131165340;
+			public const int mtrl_snackbar_background = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int navigation_empty_icon = 2131165341;
+			public const int mtrl_tabs_default_indicator = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int Nike_logo = 2131165342;
+			public const int navigate_next_24px = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int notification_action_background = 2131165343;
+			public const int navigation_empty_icon = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int notification_bg = 2131165344;
+			public const int Nike_logo = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int notification_bg_low = 2131165345;
+			public const int notification_action_background = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int notification_bg_low_normal = 2131165346;
+			public const int notification_bg = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int notification_bg_low_pressed = 2131165347;
+			public const int notification_bg_low = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int notification_bg_normal = 2131165348;
+			public const int notification_bg_low_normal = 2131165348;
 			
 			// aapt resource value: 0x7F0700A5
-			public const int notification_bg_normal_pressed = 2131165349;
+			public const int notification_bg_low_pressed = 2131165349;
 			
 			// aapt resource value: 0x7F0700A6
-			public const int notification_icon_background = 2131165350;
+			public const int notification_bg_normal = 2131165350;
 			
 			// aapt resource value: 0x7F0700A7
-			public const int notification_template_icon_bg = 2131165351;
+			public const int notification_bg_normal_pressed = 2131165351;
 			
 			// aapt resource value: 0x7F0700A8
-			public const int notification_template_icon_low_bg = 2131165352;
+			public const int notification_icon_background = 2131165352;
 			
 			// aapt resource value: 0x7F0700A9
-			public const int notification_tile_bg = 2131165353;
+			public const int notification_template_icon_bg = 2131165353;
 			
 			// aapt resource value: 0x7F0700AA
-			public const int notify_panel_notification_icon_bg = 2131165354;
+			public const int notification_template_icon_low_bg = 2131165354;
 			
 			// aapt resource value: 0x7F0700AB
-			public const int NotImage = 2131165355;
+			public const int notification_tile_bg = 2131165355;
 			
 			// aapt resource value: 0x7F0700AC
-			public const int Oviedo_logo = 2131165356;
-			
-			// aapt resource value: 0x7F0700AE
-			public const int Pantalones = 2131165358;
+			public const int notify_panel_notification_icon_bg = 2131165356;
 			
 			// aapt resource value: 0x7F0700AD
-			public const int pantalon_1 = 2131165357;
+			public const int NotImage = 2131165357;
 			
-			// aapt resource value: 0x7F0700AF
-			public const int payment_24px = 2131165359;
+			// aapt resource value: 0x7F0700AE
+			public const int Oviedo_logo = 2131165358;
 			
 			// aapt resource value: 0x7F0700B0
-			public const int person_pin_circle_24px = 2131165360;
+			public const int Pantalones = 2131165360;
+			
+			// aapt resource value: 0x7F0700AF
+			public const int pantalon_1 = 2131165359;
 			
 			// aapt resource value: 0x7F0700B1
-			public const int price_tag = 2131165361;
+			public const int payment_24px = 2131165361;
+			
+			// aapt resource value: 0x7F0700B2
+			public const int person_pin_circle_24px = 2131165362;
+			
+			// aapt resource value: 0x7F0700B3
+			public const int price_tag = 2131165363;
 			
 			// aapt resource value: 0x7F07000B
 			public const int Producto_no_disponible = 2131165195;
 			
-			// aapt resource value: 0x7F0700B2
-			public const int receipt_24px = 2131165362;
-			
-			// aapt resource value: 0x7F0700B3
-			public const int RedimensionadosBanner_1 = 2131165363;
-			
 			// aapt resource value: 0x7F0700B4
-			public const int RedimensionadosBanner_2 = 2131165364;
+			public const int receipt_24px = 2131165364;
 			
 			// aapt resource value: 0x7F0700B5
-			public const int RedimensionadosBanner_3 = 2131165365;
+			public const int RedimensionadosBanner_1 = 2131165365;
 			
 			// aapt resource value: 0x7F0700B6
-			public const int RedimensionadosBanner_4 = 2131165366;
+			public const int RedimensionadosBanner_2 = 2131165366;
 			
 			// aapt resource value: 0x7F0700B7
-			public const int RedimensionadosHueco_1 = 2131165367;
+			public const int RedimensionadosBanner_3 = 2131165367;
 			
 			// aapt resource value: 0x7F0700B8
-			public const int RedimensionadosHueco_2 = 2131165368;
+			public const int RedimensionadosBanner_4 = 2131165368;
 			
 			// aapt resource value: 0x7F0700B9
-			public const int RedimensionadosHueco_3 = 2131165369;
+			public const int RedimensionadosHueco_1 = 2131165369;
 			
 			// aapt resource value: 0x7F0700BA
-			public const int RedimensionadosHueco_4 = 2131165370;
+			public const int RedimensionadosHueco_2 = 2131165370;
 			
 			// aapt resource value: 0x7F0700BB
-			public const int RedimensionadosHueco_5 = 2131165371;
+			public const int RedimensionadosHueco_3 = 2131165371;
 			
 			// aapt resource value: 0x7F0700BC
-			public const int remove_gray_24px = 2131165372;
+			public const int RedimensionadosHueco_4 = 2131165372;
 			
 			// aapt resource value: 0x7F0700BD
-			public const int remove_purple_24px = 2131165373;
+			public const int RedimensionadosHueco_5 = 2131165373;
 			
 			// aapt resource value: 0x7F0700BE
-			public const int Rifle_logo = 2131165374;
+			public const int remove_gray_24px = 2131165374;
 			
 			// aapt resource value: 0x7F0700BF
-			public const int room_24px = 2131165375;
+			public const int remove_purple_24px = 2131165375;
 			
 			// aapt resource value: 0x7F0700C0
-			public const int round_arrow_back_purple_24px = 2131165376;
+			public const int Rifle_logo = 2131165376;
 			
 			// aapt resource value: 0x7F0700C1
-			public const int round_arrow_back_white_24px = 2131165377;
+			public const int room_24px = 2131165377;
 			
 			// aapt resource value: 0x7F0700C2
-			public const int round_arrow_white_24px = 2131165378;
+			public const int round_arrow_back_purple_24px = 2131165378;
 			
 			// aapt resource value: 0x7F0700C3
-			public const int Salud = 2131165379;
+			public const int round_arrow_back_white_24px = 2131165379;
 			
 			// aapt resource value: 0x7F0700C4
-			public const int Santafe_logo = 2131165380;
+			public const int round_arrow_white_24px = 2131165380;
 			
 			// aapt resource value: 0x7F0700C5
-			public const int search = 2131165381;
+			public const int Salud = 2131165381;
 			
 			// aapt resource value: 0x7F0700C6
-			public const int search_24px = 2131165382;
+			public const int Santafe_logo = 2131165382;
 			
 			// aapt resource value: 0x7F0700C7
-			public const int share_24px = 2131165383;
+			public const int search = 2131165383;
 			
 			// aapt resource value: 0x7F0700C8
-			public const int shopping_bag = 2131165384;
+			public const int search_24px = 2131165384;
 			
 			// aapt resource value: 0x7F0700C9
-			public const int shopping_cart_24px = 2131165385;
+			public const int share_24px = 2131165385;
+			
+			// aapt resource value: 0x7F0700CA
+			public const int shopping_bag = 2131165386;
+			
+			// aapt resource value: 0x7F0700CB
+			public const int shopping_cart_24px = 2131165387;
 			
 			// aapt resource value: 0x7F07000C
 			public const int Sin_resultados_de_busqueda = 2131165196;
 			
-			// aapt resource value: 0x7F0700CA
-			public const int Splash = 2131165386;
-			
-			// aapt resource value: 0x7F0700CB
-			public const int star_rate_24px = 2131165387;
-			
 			// aapt resource value: 0x7F0700CC
-			public const int store = 2131165388;
+			public const int Splash = 2131165388;
 			
 			// aapt resource value: 0x7F0700CD
-			public const int store_mall_directory_24px = 2131165389;
+			public const int star_rate_24px = 2131165389;
+			
+			// aapt resource value: 0x7F0700CE
+			public const int store = 2131165390;
+			
+			// aapt resource value: 0x7F0700CF
+			public const int store_mall_directory_24px = 2131165391;
 			
 			// aapt resource value: 0x7F07000D
 			public const int Tachado = 2131165197;
 			
-			// aapt resource value: 0x7F0700CE
-			public const int Tecnologia = 2131165390;
-			
-			// aapt resource value: 0x7F0700CF
-			public const int timelapse_24px = 2131165391;
-			
 			// aapt resource value: 0x7F0700D0
-			public const int tooltip_frame_dark = 2131165392;
+			public const int Tecnologia = 2131165392;
 			
 			// aapt resource value: 0x7F0700D1
-			public const int tooltip_frame_light = 2131165393;
+			public const int timelapse_24px = 2131165393;
+			
+			// aapt resource value: 0x7F0700D2
+			public const int tooltip_frame_dark = 2131165394;
+			
+			// aapt resource value: 0x7F0700D3
+			public const int tooltip_frame_light = 2131165395;
 			
 			// aapt resource value: 0x7F07000E
 			public const int ToT1 = 2131165198;
@@ -8657,50 +8663,56 @@ namespace civicamall.Droid
 			// aapt resource value: 0x7F070016
 			public const int ToT9 = 2131165206;
 			
+			// aapt resource value: 0x7F0700D4
+			public const int twitter = 2131165396;
+			
+			// aapt resource value: 0x7F0700D5
+			public const int twitter_purple_logo = 2131165397;
+			
+			// aapt resource value: 0x7F0700D6
+			public const int user_profile = 2131165398;
+			
 			// aapt resource value: 0x7F0700D8
-			public const int twitter = 2131165400;
+			public const int Vestidos = 2131165400;
 			
-			// aapt resource value: 0x7F0700D9
-			public const int user_profile = 2131165401;
-			
-			// aapt resource value: 0x7F0700DB
-			public const int Vestidos = 2131165403;
-			
-			// aapt resource value: 0x7F0700DA
-			public const int Vestido_1_1 = 2131165402;
+			// aapt resource value: 0x7F0700D7
+			public const int Vestido_1_1 = 2131165399;
 			
 			// aapt resource value: 0x7F070017
 			public const int Wsih_List = 2131165207;
 			
+			// aapt resource value: 0x7F0700D9
+			public const int youtube = 2131165401;
+			
+			// aapt resource value: 0x7F0700DA
+			public const int youtube_purple_logo = 2131165402;
+			
+			// aapt resource value: 0x7F0700DB
+			public const int zapatillas_1 = 2131165403;
+			
 			// aapt resource value: 0x7F0700DC
-			public const int youtube = 2131165404;
+			public const int zapatillas_1_1 = 2131165404;
 			
 			// aapt resource value: 0x7F0700DD
-			public const int zapatillas_1 = 2131165405;
+			public const int zapatillas_1_2 = 2131165405;
 			
 			// aapt resource value: 0x7F0700DE
-			public const int zapatillas_1_1 = 2131165406;
+			public const int Zapatillas_2 = 2131165406;
 			
 			// aapt resource value: 0x7F0700DF
-			public const int zapatillas_1_2 = 2131165407;
+			public const int Zapatillas_3 = 2131165407;
 			
 			// aapt resource value: 0x7F0700E0
-			public const int Zapatillas_2 = 2131165408;
+			public const int Zapatillas_4 = 2131165408;
 			
 			// aapt resource value: 0x7F0700E1
-			public const int Zapatillas_3 = 2131165409;
+			public const int Zapatillas_5 = 2131165409;
 			
 			// aapt resource value: 0x7F0700E2
-			public const int Zapatillas_4 = 2131165410;
+			public const int Zapatillas_6 = 2131165410;
 			
 			// aapt resource value: 0x7F0700E3
-			public const int Zapatillas_5 = 2131165411;
-			
-			// aapt resource value: 0x7F0700E4
-			public const int Zapatillas_6 = 2131165412;
-			
-			// aapt resource value: 0x7F0700E5
-			public const int Zapatillas_7 = 2131165413;
+			public const int Zapatillas_7 = 2131165411;
 			
 			static Drawable()
 			{
